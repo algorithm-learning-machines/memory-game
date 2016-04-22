929179de556d0491f05d6a16413539938d1835fc
Tudor Berariu <tudor.berariu@gmail.com>
2016-04-22 22:30:47 +0300

    Added comments in main.lua

--- main.lua
+++ main.lua
@@ -6,36 +6,70 @@ cmd:text()
 cmd:text("Playing a demo")
 cmd:text()
 cmd:text("Options:")
+
+--------------------------------------------------------------------------------
+-- Game configuration
+cmd:option("--game", "memory", "The game to play")
 cmd:option("--width", 4, "Size of game")
+
+--------------------------------------------------------------------------------
+-- Game tracking
 cmd:option("--sleep", 0, "Sleep before each action")
 cmd:option("--display", false, "Display game")
+cmd:option("--plotStates", true, "Plot evolution of states no")
+cmd:option("--plotScores", true, "Plot evolution of states no")
+cmd:option("--printScores", false, "Plot evolution of states no")
 
-cmd:option("--learning_rate", 0.1, "Learning rate")
-cmd:option("--epsLearning", 0.5, "Epsilon pentru greedy")
-cmd:option("--epsEvaluate", 0.1, "Epsilon pentru evalaure")
-cmd:option("--discout", 0.9, "Gama")
+--------------------------------------------------------------------------------
+-- Learning constraints
 cmd:option("--memorySize", 3, "Size of memory")
 
+--------------------------------------------------------------------------------
+-- Learning hiper-parameters
+cmd:option("--learningRate", 0.1, "Learning rate")
+cmd:option("--epsLearning", 0.5, "Epsilon pentru greedy")
+cmd:option("--epsEvaluate", 0, "Epsilon pentru evalaure")
+cmd:option("--discout", 0.9, "Gama")
 
+--------------------------------------------------------------------------------
+-- Training and evalutaion
 cmd:option("--episodesNo", 100000, "Number of training episodes")
 cmd:option("--evalEvery", 200, "Eval the learning every n games")
-cmd:option("-evalEpisodes", 10, "Number of episodes to use for evaluation")
+cmd:option("--evalEpisodes", 10, "Number of episodes to use for evaluation")
+
+--------------------------------------------------------------------------------
+-- Player to test
+cmd:option("--player", "rand", "Who has to play? (Q/rand/not implemented)")
 
 
-cmd:option("--player", "random", "Who has to play?")
-
+--------------------------------------------------------------------------------
+-- Parse command line arguments
 opt = cmd:parse(arg)
 
-local MemoryGame = require("memory_game")
-local player
 
-if opt.player == "random" then
-    Player = require("RandomPlayer")
-elseif opt.player == "q_learning" then
-    Player = require("QPlayer")
+--------------------------------------------------------------------------------
+-- Instantiate game
+local MemoryGame
+if opt.game == "memory" then
+   MemoryGame = require("memory_game")
+else
+   error("Not implemented yet!")
 end
 
-local player = Player.create(opt)
+--------------------------------------------------------------------------------
+-- Instantiate player
+
+if opt.player == "rand" then
+   Player = require("RandomPlayer")
+   opt.plotStates = false
+elseif opt.player == "Q" then
+   Player = require("QPlayer")
+end
+
+local player = Player(opt)
+
+--------------------------------------------------------------------------------
+-- Train and evaluate
 
 local episodesNo = tonumber(opt.episodesNo)
 local evalEvery = tonumber(opt.evalEvery)
@@ -47,69 +81,76 @@ evalScores = torch.Tensor(evalSessionsNo)
 statesNo = torch.Tensor(evalSessionsNo)
 
 for s = 1, evalSessionsNo do
-    for e = 1, evalEvery do
-        local game = MemoryGame(opt)
-        local state = game:serialize()
-        local oldState, actionsAvailable
+   -----------------------------------------------------------------------------
+   -- Train
+   for e = 1, evalEvery do
+      local game = MemoryGame(opt)
+      local state = game:serialize()
+      local oldState, actionsAvailable
 
-        while not game:isOver() do
-            oldState = state
+      while not game:isOver() do
+         if opt.display then game:display(false); sleep(opt.sleep) end
 
-            actionsAvailable = game:getAvailableActions()
+         oldState = state
+         actionsAvailable = game:getAvailableActions()
+         action = player:selectAction(oldState, actionsAvailable, true)
+         state, reward = game:applyAction(action)
+         player:feedback(oldState, action, reward, state)
 
-            action = player:selectAction(oldState, actionsAvailable, true)
+         if opt.display then game:display(true); sleep(opt.sleep) end
 
-            state, reward = game:applyAction(action)
-            player:feedback(oldState, action, reward, state)
+      end
 
-            if opt.display then game:display(true); sys.sleep(tonumber(opt.sleep))
-            end
+      if opt.printScores then print("[T]Score:" .. game.score) end
 
-        end
+      trainingScores[(s - 1) * evalEvery + e] = game.score
 
-        print("Score:" .. game.score)
+   end
 
-        trainingScores[(s - 1) * evalEvery + e] = game.score
+   -----------------------------------------------------------------------------
+   -- Evaluate
 
-    end
+   local totalScore = 0
+   statesNo[s] = player:getStatesNo()
 
-    local totalScore = 0
-    if opt.player == "q_learning" then
-        statesNo[s] = player.statesNo
-    end
+   for e = 1, evalEpisodesNo do
+      local game = MemoryGame(opt)
+      local state = game:serialize()
+      local reward, actionsAvailable
 
-    for e = 1, evalEpisodesNo do
-        local game = MemoryGame(opt)
-        local state = game:serialize()
-        local reward, actionsAvailable
+      while not game:isOver() do
+         if opt.display then game:display(false); sleep(opt.sleep) end
 
-        while not game:isOver() do
-            if opt.display then game:display(false); sys.sleep(tonumber(opt.sleep))
-            end
-            actionsAvailable = game:getAvailableActions()
-            action = player:selectAction(state, actionsAvailable, false)
+         actionsAvailable = game:getAvailableActions()
+         action = player:selectAction(state, actionsAvailable, false)
+         state, _ = game:applyAction(action)
 
-            state, _ = game:applyAction(action)
+         if opt.display then game:display(true); sleep(opt.sleep) end
+      end
 
-            if opt.display then game:display(true); sys.sleep(tonumber(opt.sleep))
-            end
-        end
+      if opt.printScores then print("[E]Score:" .. game.score) end
 
-        print("EvalScore:" .. game.score)
-        totalScore = totalScore + game.score
-    end
+      totalScore = totalScore + game.score
+   end
 
-    evalScores[s] = (totalScore / evalEpisodesNo)
+   evalScores[s] = (totalScore / evalEpisodesNo)
 
-    gnuplot.figure(1)
-    gnuplot.plot(
-        {'Train', torch.linspace(1, s * evalEvery, s * evalEvery), trainingScores[{{1, s * evalEvery}}], "-"},
-        {'Eval', torch.linspace(evalEvery, s * evalEvery, s), evalScores[{{1, s}}], "~"})
-
-    if opt.player == "q_learning" then
-        gnuplot.figure(2)
-        gnuplot.plot(
-      {'Eval', statesNo[{{1, s}}], "~"}
+   -----------------------------------------------------------------------------
+   -- Plot scores
+   if opt.plotScores then
+      gnuplot.figure(1)
+      local idx = s * evalEvery                                 -- current index
+      gnuplot.plot(
+         {'Train', torch.linspace(1, idx, idx), trainingScores[{{1,idx}}], "-"},
+         {'Eval', torch.linspace(evalEvery, idx, s), evalScores[{{1,s}}], "~"}
       )
-    end
+   end
+
+   -----------------------------------------------------------------------------
+   -- Plot # of states
+   if opt.plotStates then
+      gnuplot.figure(2)
+      gnuplot.plot({'Eval', statesNo[{{1,s}}], "~"})
+   end
+
 end
