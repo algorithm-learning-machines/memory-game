a26bc24230e255667114a6a7ff11536c526c537b
andrada <bianca.andrada.cretan@gmail.com>
2016-09-06 17:16:01 +0300

    delete some functions

--- SARSAPlayer.lua
+++ SARSAPlayer.lua
@@ -15,38 +15,12 @@ function SARSAPlayer:__init(opt)
    self.learningRate = tonumber(opt.learningRate) or 0.1
    self.discount = tonumber(opt.discount) or 0.99
    self.memorySize = tonumber(opt.memorySize) or 3
-   self.idxCrt = 1
    self.oldStates =
    comp 'table(y, "" for y)' (seq.copy(seq.range(1,self.memorySize)))
 
    self.statesNo = 0
    self.lastAction = ""
 end
-
-function SARSAPlayer.getIndex(n, m)
-   return (n - 1) % m + 1
-end
-
-function SARSAPlayer:createMemory()
-   if self.memorySize == 0 then
-      return nil
-   end
-
-   local memory = ""
-
-   local ind = self.getIndex(self.idxCrt, self.memorySize)
-
-   for i = ind - 1, 1, -1 do
-      memory = (memory .. self.oldStates[i])
-   end
-
-   for i = self.memorySize, ind, -1 do
-      memory = (memory .. self.oldStates[i])
-   end
-
-   return memory
-end
-
 
 function SARSAPlayer:selectAction(state, actions, isTraining)
    if (not isTraining and math.random() >= self.epsEvaluate)
@@ -58,21 +32,6 @@ function SARSAPlayer:selectAction(state, actions, isTraining)
 end
 
 function SARSAPlayer:bestAction(state, actions)
-   if self.memorySize > 0 then
-      for i = 1, self.memorySize - 1 do
-         for l in string.gmatch(self.oldStates[i], "%a") do
-            local pos = string.find(self.oldStates[i], l)
-
-            for j = i + 1, self.memorySize do
-               local pos2 = string.find(self.oldStates[j], l)
-               if pos2 and pos ~= pos2 then
-                  return getString(pos, pos2)
-               end
-            end
-         end
-      end
-   end
-
    local Qs = self.Q[state] or {}
    local bestAction
    local bestQ
@@ -93,7 +52,6 @@ function SARSAPlayer:bestAction(state, actions)
 end
 
 function SARSAPlayer:feedback(state, action, reward, nextState, newAction)
-   local ind = self.getIndex(self.idxCrt, self.memorySize)
    local q
 
    if not self.Q[state] then
@@ -113,16 +71,11 @@ function SARSAPlayer:feedback(state, action, reward, nextState, newAction)
    self.Q[state][action] = self.Q[state][action] + self.learningRate *
       (reward + self.discount * q - self.Q[state][action])
 
-   if self.memorySize >= 1 then
-      self.oldStates[ind] = nextState
-   end
-   self.idxCrt = self.idxCrt + 1
 end
 
 
 function SARSAPlayer:getStatesNo()
    return self.statesNo
 end
-
 
 return SARSAPlayer
